c0725c64176ddfd2059110408f5c95cbdc86defe
Kyle Rego <regoky@outlook.com>
2024-08-29 11:50:58 -0400

    Add Create unit conversions table migration

---
+++ WebApi/Migrations/20240829155030_AddUnitConversions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Larder.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnitConversions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnitConversions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    UnitId = table.Column<string>(type: "TEXT", nullable: false),
+                    TargetUnitId = table.Column<string>(type: "TEXT", nullable: false),
+                    TargetUnitsPerUnit = table.Column<double>(type: "REAL", nullable: false),
+                    UnitType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnitConversions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UnitConversions_Units_TargetUnitId",
+                        column: x => x.TargetUnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UnitConversions_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnitConversions_TargetUnitId",
+                table: "UnitConversions",
+                column: "TargetUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnitConversions_UnitId",
+                table: "UnitConversions",
+                column: "UnitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnitConversions");
+        }
+    }
+}
