6ae1b110021ff2db1c4538d33c71d3a80305c451
Kyle Rego <regoky@outlook.com>
2025-02-20 16:40:59 -0800

    Add migration simplifying recipe-item many to many

---
+++ Larder/Migrations/20250221004013_ChangeRecipeIngredientsRelationship.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Larder.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRecipeIngredientsRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeIngredients_Ingredients_IngredientId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropTable(
+                name: "IngredientRecipe");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IngredientId",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IngredientId",
+                table: "RecipeIngredients",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemId",
+                table: "RecipeIngredients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ItemRecipe",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<string>(type: "TEXT", nullable: false),
+                    RecipesId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemRecipe", x => new { x.IngredientsId, x.RecipesId });
+                    table.ForeignKey(
+                        name: "FK_ItemRecipe_Items_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemRecipe_Recipes_RecipesId",
+                        column: x => x.RecipesId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_IngredientId",
+                table: "Recipes",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredients_ItemId",
+                table: "RecipeIngredients",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemRecipe_RecipesId",
+                table: "ItemRecipe",
+                column: "RecipesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeIngredients_Ingredients_IngredientId",
+                table: "RecipeIngredients",
+                column: "IngredientId",
+                principalTable: "Ingredients",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeIngredients_Items_ItemId",
+                table: "RecipeIngredients",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Ingredients_IngredientId",
+                table: "Recipes",
+                column: "IngredientId",
+                principalTable: "Ingredients",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeIngredients_Ingredients_IngredientId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeIngredients_Items_ItemId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Ingredients_IngredientId",
+                table: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "ItemRecipe");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_IngredientId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeIngredients_ItemId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropColumn(
+                name: "IngredientId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IngredientId",
+                table: "RecipeIngredients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IngredientRecipe",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<string>(type: "TEXT", nullable: false),
+                    RecipesId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientRecipe", x => new { x.IngredientsId, x.RecipesId });
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Ingredients_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Recipes_RecipesId",
+                        column: x => x.RecipesId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientRecipe_RecipesId",
+                table: "IngredientRecipe",
+                column: "RecipesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeIngredients_Ingredients_IngredientId",
+                table: "RecipeIngredients",
+                column: "IngredientId",
+                principalTable: "Ingredients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
