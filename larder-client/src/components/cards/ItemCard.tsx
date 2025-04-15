697f9fc8d16f7f88db093858fb9635a262c0b237
Kyle Rego <regoky@outlook.com>
2025-04-15 14:45:48 -0700

    Add default item image for items with no thumbnail

--- larder-client/src/components/cards/ItemCard.tsx
+++ larder-client/src/components/cards/ItemCard.tsx
@@ -3,15 +3,28 @@ import QuantitySpan from "../QuantitySpan";
 
 export default function ItemCard({item} : {item: ItemDto}) {
     return <div className="card" style={{maxWidth: "243px", wordBreak: "break-all"}}>
-                <div className="card-body">
-                    <h2 className="card-title fs-5">
-                        {item.quantity && <QuantitySpan quantity={item.quantity} />} {item.name}
+                <div className="card-body d-flex flex-column row-gap-1">
+                    <h2 className="fs-4">
+                        {item.name}
                     </h2>
+
+                    <h3 className="card-title fs-5">
+                        {item.quantity && <QuantitySpan quantity={item.quantity} />}
+                    </h3>
+
+                    {item.description &&
                     <p className="card-text">
                         {item.description}
                     </p>
+                    }
+
                     <div className="d-flex justify-content-center">
-                        <img src={`${import.meta.env.VITE_API_ORIGIN}/api/Items/${item.id}/image`} alt="Item Image" />
+                        <img src={`${import.meta.env.VITE_API_ORIGIN}/api/Items/${item.id}/image`}
+                            onError={(e) => {
+                            e.currentTarget.onerror = null;
+                            e.currentTarget.src = "/default-item-image.png";
+                        }}
+                            alt="Item Image" />
                     </div>
                 </div>
             </div>;
