90b98d53c184e8d3c577e0e1671c95d97b8eb9d4
Kyle Rego <regoky@outlook.com>
2025-04-15 14:48:36 -0700

    Small change to item card

--- larder-client/src/components/cards/ItemCard.tsx
+++ larder-client/src/components/cards/ItemCard.tsx
@@ -4,19 +4,13 @@ import QuantitySpan from "../QuantitySpan";
 export default function ItemCard({item} : {item: ItemDto}) {
     return <div className="card" style={{maxWidth: "243px", wordBreak: "break-all"}}>
                 <div className="card-body d-flex flex-column row-gap-1">
-                    <h2 className="fs-4">
+                    <h2 className="fs-4 text-center">
                         {item.name}
                     </h2>
 
-                    <h3 className="card-title fs-5">
-                        {item.quantity && <QuantitySpan quantity={item.quantity} />}
+                    <h3 className="card-title fs-6">
+                        Quantity: {item.quantity && <QuantitySpan quantity={item.quantity} />}
                     </h3>
-
-                    {item.description &&
-                    <p className="card-text">
-                        {item.description}
-                    </p>
-                    }
 
                     <div className="d-flex justify-content-center">
                         <img src={`${import.meta.env.VITE_API_ORIGIN}/api/Items/${item.id}/image`}
@@ -26,6 +20,12 @@ export default function ItemCard({item} : {item: ItemDto}) {
                         }}
                             alt="Item Image" />
                     </div>
+
+                    {item.description &&
+                    <p className="card-text text-center">
+                        {item.description}
+                    </p>
+                    }
                 </div>
             </div>;
 }
